d2c40aa4d6c3ea9f286cd8c57ac27870e3f3e207
Ubuntu <ubuntu@ip-172-31-31-251.(none)>
2013-07-10 15:15:57 +0000

    FS Handle

--- web.js
+++ web.js
@@ -1,5 +1,5 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 fs.readFile('index.html', function (err, data) {
@@ -8,9 +8,12 @@ fs.readFile('index.html', function (err, data) {
 		response.send(data);
 	});
 });
-//app.get('/', function(request, response) {
-//  response.send('Hello World2!');
-//});
+app.get('/', function(request, response) {
+	var rawdata = fs.readFileSync("index.html","utf-8");
+	var bufferhandle = new Buffer(rawdata); 
+	response.send(bufferhandle.toString());
+	console.log('>'= bufferhandle.toString());
+});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
